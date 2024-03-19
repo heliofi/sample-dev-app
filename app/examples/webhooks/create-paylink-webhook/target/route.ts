42bfad39bb69a40b18838166e04547837ef8b3df
00MB <michaelwbeer@gmail.com>
2024-03-19 09:58:29 +0000

    fix webhook secret

--- app/examples/webhooks/create-paylink-webhook/target/route.ts
+++ app/examples/webhooks/create-paylink-webhook/target/route.ts
@@ -1,9 +1,10 @@
 export async function POST(req: Request, res: Response) {
   const authHeader = req.headers.get("authorization");
+  const webhookSecret = '' // This is given to you in the response when you first create a webhook
   // You can use the Bearer token to authenticate the request is coming from Helio
   console.log(authHeader);
 
-  if (authHeader !== `Bearer ${process.env.SECRET_API_KEY}`) {
+  if (authHeader !== `Bearer ${webhookSecret}`) {
     return new Response("Unauthorized", {
       status: 401,
     });
